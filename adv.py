92ff0e329409e6817467a2379fce0e3b8d1e2492
Douglas Campbell <dacampbelltbone@gmail.com>
2020-04-18 12:55:04 -0400

    Created DFT and method to implement BFS.

--- adv.py
+++ adv.py
@@ -26,6 +26,60 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+#### Explore rooms and fill Graph ####
+# instantiate Graph
+g = Graph()
+
+start_rm = world.starting_room
+
+## Implement Depth First Traversal ##
+stk = Stack()
+stk.push(start_rm)
+visited = set()
+while stk.size() > 0:
+    rm = stk.pop()
+    rm_id = rm.id
+    if rm_id not in g.vertices:
+        g.add_vertex(rm_id)
+
+    rm_connected = rm.getExits()
+    for drt in rm_connected:
+        rm_connected = rm.get_room_in_direction(drt)
+        rm_connected_id = rm_connected.id
+        if rm_connected_id not in g.vertices:
+            g.add_vertex(rm_connected_id)
+        g.add_edge(rm_id, rm_connected_id, drt)
+        if rm_connected_id not in visited:
+            stk.push(rm_connected)
+    visited.add(rm_id)
+
+
+## Method to implement Breadth First Search ##
+def bfs_search_rooms(rm_id, visited, g=g):
+    rms_moves = {}
+    rms_moves[rm_id] = [[rm_id], []]
+    q = Queue()
+    q.enqueue([[rm_id], []])
+    while q.size() > 0:
+        rms, moves = q.dequeue()
+        id_last_rm = rms[-1]
+        adj = g.get_neighbors(id_last_rm)
+        keys_adj = list(adj.keys())
+        if len(keys_adj) == 1 and adj[keys_adj[0]] not in visited:
+            not_explored_path_de = list(moves) + [keys_adj[0]]
+            return not_explored_path_de
+        else:
+            for card_dir in adj:
+                nxt_rm = adj[card_dir]
+                new_rms = list(rms) + [nxt_rm]
+                new_moves = list(moves) + [card_dir]
+                if nxt_rm not in rms_moves:
+                    q.enqueue([new_rms, new_moves])
+                    rms_moves[nxt_rm] = [new_rms, new_moves]
+                if nxt_rm not in visited:
+                    return new_moves
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
